8dfe3606c5d605e40225d0f0ef0c13cf81736fc3
Nikunj Ramani <nikunjramani7624@gmail.com>
2025-03-04 12:07:57 +0530

    reformat code

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -1,7 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-
 import 'intl/messages_all.dart';
 
 // **************************************************************************
@@ -19,17 +18,17 @@ class LocalString {
   static LocalString? _current;
 
   static LocalString get current {
-    assert(
-      _current != null,
-      'No instance of LocalString was loaded. Try to initialize the LocalString delegate before accessing LocalString.current.',
-    );
+    assert(_current != null,
+        'No instance of LocalString was loaded. Try to initialize the LocalString delegate before accessing LocalString.current.');
     return _current!;
   }
 
   static const AppLocalizationDelegate delegate = AppLocalizationDelegate();
 
   static Future<LocalString> load(Locale locale) {
-    final name = (locale.countryCode?.isEmpty ?? false) ? locale.languageCode : locale.toString();
+    final name = (locale.countryCode?.isEmpty ?? false)
+        ? locale.languageCode
+        : locale.toString();
     final localeName = Intl.canonicalizedLocale(name);
     return initializeMessages(localeName).then((_) {
       Intl.defaultLocale = localeName;
@@ -42,7 +41,8 @@ class LocalString {
 
   static LocalString of(BuildContext context) {
     final instance = LocalString.maybeOf(context);
-    assert(instance != null, 'No instance of LocalString present in the widget tree. Did you add LocalString.delegate in localizationsDelegates?');
+    assert(instance != null,
+        'No instance of LocalString present in the widget tree. Did you add LocalString.delegate in localizationsDelegates?');
     return instance!;
   }
 
@@ -52,12 +52,22 @@ class LocalString {
 
   /// `Authentication Failure`
   String get authentication_failure {
-    return Intl.message('Authentication Failure', name: 'authentication_failure', desc: '', args: []);
+    return Intl.message(
+      'Authentication Failure',
+      name: 'authentication_failure',
+      desc: '',
+      args: [],
+    );
   }
 
   /// `Server Error`
   String get server_error {
-    return Intl.message('Server Error', name: 'server_error', desc: '', args: []);
+    return Intl.message(
+      'Server Error',
+      name: 'server_error',
+      desc: '',
+      args: [],
+    );
   }
 }
 
@@ -65,15 +75,15 @@ class AppLocalizationDelegate extends LocalizationsDelegate<LocalString> {
   const AppLocalizationDelegate();
 
   List<Locale> get supportedLocales {
-    return const <Locale>[Locale.fromSubtags(languageCode: 'en')];
+    return const <Locale>[
+      Locale.fromSubtags(languageCode: 'en'),
+    ];
   }
 
   @override
   bool isSupported(Locale locale) => _isSupported(locale);
-
   @override
   Future<LocalString> load(Locale locale) => LocalString.load(locale);
-
   @override
   bool shouldReload(AppLocalizationDelegate old) => false;
 
